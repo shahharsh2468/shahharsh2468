ac66124439df760e56449e1e1bc1d4098056777a
shahharsh2468 <shahharsh2468@gmail.com>
2023-06-20 21:42:23 +0530

    #adding ajax request to add expense

--- api/app.js
+++ api/app.js
@@ -2,13 +2,18 @@
 const express = require("express");
 const db = require("./database/conn");
 const bodyParser = require('body-parser');
+const cors= require('cors');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
-// app.use(cors());
+app.use(cors({
+  origin: 'http://127.0.0.1:8080',
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: 'Content-Type',
+}));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080/'); // Replace with the URL of your front-end application
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -25,3 +30,4 @@ app.listen(port, () => {
 module.exports = app;
 
 require('./controller/UserController');
+require('./controller/ExpenseController');
--- api/controller/ExpenseController.js
+++ api/controller/ExpenseController.js
@@ -4,11 +4,22 @@ const Expenses = require('../model/ExpenseModel');
 
 app.post("/expenses",async(req,res)=> {
     try{
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
         if(Object.keys(req.body).length == 0){
             return response.message(res,400,"ERROR",'no data in body'); //if there is no content in body then this error will generate
         }else{
-            const Expenses = new Expenses(req.body);
-            const createExpenses = await Expenses.save();
+            const expense = new Expenses(req.body);
+            const createExpenses = await expense.save();
             response.message(res,201,"SUCCESS",'Expense created successfully',createExpenses); //successfully created new customer
         }
     } catch(e) {
@@ -23,7 +34,7 @@ app.get("/expenses",async(req,res)=> {
         if(Object.keys(Expenses).length == 0){
             return response.message(res,200,"SUCCESS",'No Data Available'); //if there is no content in body then this error will generate
         }
-        response.message(res,200,"SUCCESS",'Expense created successfully',createExpenses); //successfully created new customer
+        response.message(res,200,"SUCCESS",'Expense fetched successfully',Expenses); //successfully created new customer
     } catch(e) {
         response.error400(e,res);
     }
